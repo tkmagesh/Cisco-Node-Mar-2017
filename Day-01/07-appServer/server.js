be3733718b3719d25ed0f8de1b583d10f4d4d509
Magesh <tkmagesh77@gmail.com>
2017-03-16 15:35:51 +0530

    app server

---
+++ Day-01/07-appServer/server.js
@@ -0,0 +1,52 @@
+
+var http = require('http'),
+	fs = require('fs'),
+	path = require('path'),
+	url = require('url'),
+	querystring = require('querystring'),
+	calculator = require('./calculator');
+
+var staticExtns = ['.html', '.xml', '.js', '.jpg', '.png', '.css', '.img', '.json', '.txt'];
+function isStatic(resourceName){
+	return staticExtns.indexOf(path.extname(resourceName)) !== -1;
+}
+
+var server = http.createServer(function(req, res){
+	var urlObj = url.parse(req.url, true);
+	if (isStatic(urlObj.pathname)){
+		var resource = path.join(__dirname, urlObj.pathname);
+		if (!fs.existsSync(resource)){
+			res.statusCode = 404;
+			res.end();
+			return;
+		}
+		fs.createReadStream(resource).pipe(res);
+	} else if (urlObj.pathname === '/calculator' && req.method === 'GET'){
+		var reqData = urlObj.query,
+			n1 = parseInt(reqData.n1, 10),
+			op = reqData.op,
+			n2 = parseInt(reqData.n2, 10),
+			result = calculator[op](n1, n2);
+		res.write(result.toString());
+		res.end();
+	} else if (urlObj.pathname === '/calculator' && req.method === 'POST'){
+		var inputData = '';
+		req.on('data', function(chunk){
+			inputData += chunk;
+		});
+		req.on('end', function(){
+			var reqData = querystring.parse(inputData),
+				n1 = parseInt(reqData.n1, 10),
+				op = reqData.op,
+				n2 = parseInt(reqData.n2, 10),
+				result = calculator[op](n1, n2);
+			res.write(result.toString());
+			res.end();
+		})
+		
+	} else {
+		res.statusCode = 404;
+		res.end();
+	}
+});
+server.listen(8080);
